6e3203e0f923984a6bbb462fe5c24a59fb9d841b
Shia Wei-Chung <64996834+weibiomed@users.noreply.github.com>
2025-04-03 21:23:36 +0800

    modified:   app/layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
-import { Header } from '../components/header';
+import Header from '../components/header';
 
 export const metadata = {
     title: {
